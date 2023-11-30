b8d6e7d5b9ba0ece6c803f4a4cf1967b6e5d201e
Nur Nisrina Ningrum <nisrina.ningrum@shopee.com>
2023-11-30 20:52:03 +0700

    feat: add functionality of add model button in AddProduct page

--- src/components/AddProductModel.tsx
+++ src/components/AddProductModel.tsx
@@ -10,12 +10,17 @@ import toast from "react-hot-toast";
 export default function AddProductModel(): JSX.Element {
   const dispatch = useDispatch();
   const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+  const [modelCount, setModelCount] = useState<number>(1);
 
   const handleNext = () => {
     dispatch(nextPage());
   };
   const handlePrev = () => {
     dispatch(prevPage());
+  };
+
+  const addModel = () => {
+    setModelCount(modelCount + 1); // Increment the model count
   };
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -37,51 +42,61 @@ export default function AddProductModel(): JSX.Element {
     <>
       <div className="overflow-x-auto">
         <form onSubmit={handleNext}>
-          <div className="flex flex-col items-left justify-start p-8 rounded-xl border-stone-100 border-2 my-10">
-            <MainHeadline title="Model" isSupportingContent={false} />
-            <div className="my-6">
-              <label className="block text-[#666]">Model Name</label>
-              <input
-                type="text"
-                minLength={10}
-                name=""
-                id=""
-                placeholder="ex: kayu jati mod"
-                className="w-full px-4 py-3 rounded-lg mt-2 bg-[#F1F1F1]"
-                required
-              />
-            </div>
-            <div className="my-6">
-              <label className="block text-xl text-[#666]">Product Image</label>
-              <input
-                type="file"
-                multiple
-                accept=".png, .jpeg"
-                onChange={handleFileChange}
-                disabled={selectedFiles.length >= 5}
-              ></input>
-              <div>
-                {selectedFiles.map((file, index) => (
-                  <div key={index}>{file.name}</div>
-                ))}
+          {[...Array(modelCount)].map((_, index) => (
+            <div
+              key={index}
+              id={`model-${index + 1}`}
+              className="flex flex-col items-left justify-start p-8 rounded-xl border-stone-100 border-2 my-10"
+            >
+              <MainHeadline title="Model" isSupportingContent={false} />
+
+              <div className="my-6">
+                <label className="block text-[#666]">Model Name</label>
+                <input
+                  type="text"
+                  minLength={10}
+                  name=""
+                  id=""
+                  placeholder="ex: kayu jati mod"
+                  className="w-full px-4 py-3 rounded-lg mt-2 bg-[#F1F1F1]"
+                  required
+                />
+              </div>
+              <div className="my-6">
+                <label className="block text-xl text-[#666]">
+                  Product Image
+                </label>
+                <input
+                  type="file"
+                  multiple
+                  accept=".png, .jpeg"
+                  onChange={handleFileChange}
+                  disabled={selectedFiles.length >= 5}
+                ></input>
+                <div>
+                  {selectedFiles.map((file, index) => (
+                    <div key={index}>{file.name}</div>
+                  ))}
+                </div>
+              </div>
+              <div className="my-6">
+                <label className="block text-[#666]">Stock</label>
+                <input
+                  type="number"
+                  min={1}
+                  max={999}
+                  name=""
+                  id=""
+                  placeholder="ex: kayu jati mod"
+                  className="w-full px-4 py-3 rounded-lg mt-2 bg-[#F1F1F1]"
+                  required
+                />
               </div>
             </div>
-            <div className="my-6">
-              <label className="block text-[#666]">Stock</label>
-              <input
-                type="number"
-                min={1}
-                max={999}
-                name=""
-                id=""
-                placeholder="ex: kayu jati mod"
-                className="w-full px-4 py-3 rounded-lg mt-2 bg-[#F1F1F1]"
-                required
-              />
-            </div>
-          </div>
+          ))}
+
           <div className="flex justify-end">
-            <ButtonSecondary text="Add Model" />
+            <ButtonSecondary text="Add Model" action={addModel} />
           </div>
           <ButtonPagination
             left="Prev"
